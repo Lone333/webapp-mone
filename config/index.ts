37e696ffc8e92720f48c5e6419403132a4689a17
Lone <cullen.zh@gmail.com>
2023-10-20 15:33:11 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,10 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'Pi',
+  description: '嘿！👋 我是 Pi-您的私人人工智能助理。
+我随时为您提供帮助，满足您的任何需求-只需提出即可！😊',
+  copyright: 'AxzAI',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
